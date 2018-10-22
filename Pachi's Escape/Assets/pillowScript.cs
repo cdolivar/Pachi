08ef83b5c67c4f8db4bdfae9aa55e5ec15b401e6
cdolivar <charlesdavidolivar@gmail.com>
2018-10-21 19:28:10 -0700

    working on kitchen

---
+++ Pachi's Escape/Assets/pillowScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pillowScript : MonoBehaviour {
+    public Sprite remote;
+    public Sprite noRemote;
+    private int count;
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        count = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // change sprite on click
+    void OnMouseDown()
+    {   
+        if (count == 0) {
+            spriteRenderer.sprite = remote;
+            count += 1;
+        }
+        else if (count == 1) {
+            spriteRenderer.sprite = noRemote;
+            globalVariables.setTrue(0);
+        }
+
+    }
+
+
+
+}
